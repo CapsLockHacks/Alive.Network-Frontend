a7397b90be64474d99280555786419da0ea001c5
Ronak Doshi <ronak01doshi@gmail.com>
2018-08-12 16:41:37 +0530

    Bug fixes

--- main.js
+++ main.js
@@ -122,10 +122,11 @@ $(document).ready(function () {
                 processData: false,
                 mimeType: "multipart/form-data",
                 success: function (result) {
-                  if(result.result===true){
+                  if(result.result==="true"){
                   console.log(result);
                   status = "success";
                   M.toast({ html: "File successfully uploaded!" })
+                  $("#submitToAPI").hide();
 
                 }
                 },
