b4b46d9d9fcded593a862c09f69b24ba5e43371f
Ronak Doshi <ronak01doshi@gmail.com>
2018-08-12 14:59:13 +0530

    Form input fields, validation and UI

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
     $("#alert").hide()
     $("#upload").hide()
     $("#video").hide()
-    
+    $("#submitToAPI").hide();
+    $("#alias").hide();
+
     // set functions
     $("#generate").click(function () {
         var seed = $("#seed").val();
@@ -26,18 +28,18 @@ $(document).ready(function () {
                 "processData": false,
                 "data": "{ 'password': '"+seed+"' }"
             }
-            
+
             $.ajax(settings).done(function (response) {
                 console.log(response);
             });
         }
     })
-    
+
     $("#checkin").click(function (){
         var wallet = $("#wallet").val();
         var seed = $("#seed").val();
         if (seed === '' || wallet==='') {
-            M.toast({ html: "please enter some credentials to checkin!" })
+            M.toast({ html: "please enter credentials to checkin!" })
             // $("#alert").html("please enter some credentials to checkin!")
             // $("#alert").show()
         }
@@ -45,47 +47,60 @@ $(document).ready(function () {
             $("#alert").hide()
             var status = "success"
             // TODO: call api to checkin heartbeat and set status
-            
+
             var request = $.ajax({
                 url: "script.php",
                 type: "POST",
                 data: {id : "menuId"},
                 dataType: "html"
             });
-            
+
             request.done(function(msg) {
                 $("#log").html( msg );
+                status = "success"
             });
-            
-            
+
+
             request.fail(function(jqXHR, textStatus) {
                 M.toast({ html: "Request failed: " + textStatus })
                 // alert(  );
             });
-            
+
             if (status === "success"){
                 $("#upload").show()
+                $("#alias").show()
                 $("#login").hide()
                 $("#checkinBtn").hide()
+                $("#submitToAPI").show();
+
+
+                $("#submitToAPI").click(function(){
+                  if((!("$alias1").val&&!("$alias").val)||!("$heartbeatDays").val){
+                      M.toast({ html: "Please enter required details!" })
+                  }
+                  else{
+          //          TODO: API Call
+                  }
+                })
             } else{
-                M.toast({ html: "invalid credentials to checkin!" })
+                M.toast({ html: "Invalid checkin credentials!" })
 
                 // $("#alert").html("invalid credentials to checkin!")
                 // $("#alert").show()
             }
         }
     })
-    
+
     $("#uploadButton").click(function(){
         $("#fileInput").click()
     })
-    
+
     $("#record").click(function(){
         $("#buttons").hide()
         $("#video").show()
         startFunction()
     })
-    
+
     $("#uploadToChainButton").click(function () {
         downloadVideo()
     })
